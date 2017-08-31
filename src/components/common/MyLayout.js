f0cbcca125c2c9b6377f0e760f33bdcc4ef79b0a
xuatz <xzlow10@gmail.com>
2017-08-31 12:20:54 +0800

    *WIP* considering to stop using `grid-styled`

--- src/components/common/MyLayout.js
+++ src/components/common/MyLayout.js
@@ -3,43 +3,51 @@ import axios from "axios";
 
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { Half, Quarter } from "grid-styled";
+import { Flex, Box, Half, Quarter } from "grid-styled";
 
 //======================
 //consider to relocate if necessary
 const Icon = styled(Link)`
-	font-size: 20px;
-	text-decoration: none;
-	color: black;
+    font-size: 20px;
+    text-decoration: none;
+    color: black;
 `;
-const Header = props =>
-    <div>
-        <Half><Icon to="/">React Boilerplate 2017</Icon></Half>
-        <Half>
-            <Quarter> <Link to="/">Home</Link>{" "}
-            </Quarter>
-            <Quarter><Link to="/about">About</Link></Quarter>
-            <Quarter>
-                <a
-                    href="/"
-                    onClick={() => {
-                        let API = axios.create({
-                            baseURL:
-                                process.env.REACT_APP_API_URL ||
+const Header = props => (
+    <Flex>
+        <Box width={1 / 2}>
+            <Icon to="/">React Boilerplate 2017</Icon>
+        </Box>
+        <Box width={1 / 2}>
+            <Flex>
+                <Box width={1 / 3}>
+                    <Link to="/">Home</Link>
+                </Box>
+                <Box width={1 / 3}>
+                    <Link to="/about">About</Link>
+                </Box>
+                <Box width={1 / 3}>
+                    <a
+                        href="/"
+                        onClick={() => {
+                            let API = axios.create({
+                                baseURL:
+                                    process.env.REACT_APP_API_URL ||
                                     "http://localhost:9000",
-                            withCredentials: true,
-                            timeout: 5000
-                        });
+                                withCredentials: true,
+                                timeout: 5000
+                            });
 
-                        API.get("/auth/logout").then(res => {
-                            console.log(res);
-                        });
-                    }}>
-                    Logout
-                </a>
-            </Quarter>
-        </Half>
-    </div>;
+                            API.get("/auth/logout").then(res => {
+                                console.log(res);
+                            });
+                        }}>
+                        Logout
+                    </a>
+                </Box>
+            </Flex>
+        </Box>
+    </Flex>
+);
 
 //======================
 
@@ -48,11 +56,12 @@ const layoutStyle = {
     padding: "15px 10px"
     // border: '1px solid #DDD'
 };
-const Layout = props =>
+const Layout = props => (
     <div style={layoutStyle}>
         <Header />
         <hr />
         {props.children}
-    </div>;
+    </div>
+);
 
 export default Layout;
